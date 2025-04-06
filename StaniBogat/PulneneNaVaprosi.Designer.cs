83a42cb43bb77c97748a8c0fdbb2821e1fd9fae6
Lupova19 <nadiqlupova@gmail.com>
2025-04-06 22:32:12 +0300

    Update

--- StaniBogat/PulneneNaVaprosi.Designer.cs
+++ StaniBogat/PulneneNaVaprosi.Designer.cs
@@ -29,6 +29,14 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label idLabel;
+            System.Windows.Forms.Label questionLabel;
+            System.Windows.Forms.Label ansALabel;
+            System.Windows.Forms.Label ansBLabel;
+            System.Windows.Forms.Label ansCLabel;
+            System.Windows.Forms.Label ansDLabel;
+            System.Windows.Forms.Label correctAnswerLabel;
+            System.Windows.Forms.Label pointsLabel;
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.questionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -44,6 +52,23 @@
             this.btnCreate = new System.Windows.Forms.Button();
             this.btnUpdate = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
+            this.tableAdapterManager = new StaniBogat.Stani_Bogat_11d_2025DataSetTableAdapters.TableAdapterManager();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.questionTextBox = new System.Windows.Forms.TextBox();
+            this.ansATextBox = new System.Windows.Forms.TextBox();
+            this.ansBTextBox = new System.Windows.Forms.TextBox();
+            this.ansCTextBox = new System.Windows.Forms.TextBox();
+            this.ansDTextBox = new System.Windows.Forms.TextBox();
+            this.correctAnswerTextBox = new System.Windows.Forms.TextBox();
+            this.pointsTextBox = new System.Windows.Forms.TextBox();
+            idLabel = new System.Windows.Forms.Label();
+            questionLabel = new System.Windows.Forms.Label();
+            ansALabel = new System.Windows.Forms.Label();
+            ansBLabel = new System.Windows.Forms.Label();
+            ansCLabel = new System.Windows.Forms.Label();
+            ansDLabel = new System.Windows.Forms.Label();
+            correctAnswerLabel = new System.Windows.Forms.Label();
+            pointsLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.questionsBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.stani_Bogat_11d_2025DataSet)).BeginInit();
@@ -63,59 +88,77 @@
             this.correctAnswerDataGridViewTextBoxColumn,
             this.pointsDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.questionsBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(1, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(-6, -2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(804, 119);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(1072, 146);
             this.dataGridView1.TabIndex = 0;
             // 
             // idDataGridViewTextBoxColumn
             // 
             this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
             this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
             this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 125;
             // 
             // questionDataGridViewTextBoxColumn
             // 
             this.questionDataGridViewTextBoxColumn.DataPropertyName = "Question";
             this.questionDataGridViewTextBoxColumn.HeaderText = "Question";
+            this.questionDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.questionDataGridViewTextBoxColumn.Name = "questionDataGridViewTextBoxColumn";
+            this.questionDataGridViewTextBoxColumn.Width = 125;
             // 
             // ansADataGridViewTextBoxColumn
             // 
             this.ansADataGridViewTextBoxColumn.DataPropertyName = "AnsA";
             this.ansADataGridViewTextBoxColumn.HeaderText = "AnsA";
+            this.ansADataGridViewTextBoxColumn.MinimumWidth = 6;
             this.ansADataGridViewTextBoxColumn.Name = "ansADataGridViewTextBoxColumn";
+            this.ansADataGridViewTextBoxColumn.Width = 125;
             // 
             // ansBDataGridViewTextBoxColumn
             // 
             this.ansBDataGridViewTextBoxColumn.DataPropertyName = "AnsB";
             this.ansBDataGridViewTextBoxColumn.HeaderText = "AnsB";
+            this.ansBDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.ansBDataGridViewTextBoxColumn.Name = "ansBDataGridViewTextBoxColumn";
+            this.ansBDataGridViewTextBoxColumn.Width = 125;
             // 
             // ansCDataGridViewTextBoxColumn
             // 
             this.ansCDataGridViewTextBoxColumn.DataPropertyName = "AnsC";
             this.ansCDataGridViewTextBoxColumn.HeaderText = "AnsC";
+            this.ansCDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.ansCDataGridViewTextBoxColumn.Name = "ansCDataGridViewTextBoxColumn";
+            this.ansCDataGridViewTextBoxColumn.Width = 125;
             // 
             // ansDDataGridViewTextBoxColumn
             // 
             this.ansDDataGridViewTextBoxColumn.DataPropertyName = "AnsD";
             this.ansDDataGridViewTextBoxColumn.HeaderText = "AnsD";
+            this.ansDDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.ansDDataGridViewTextBoxColumn.Name = "ansDDataGridViewTextBoxColumn";
+            this.ansDDataGridViewTextBoxColumn.Width = 125;
             // 
             // correctAnswerDataGridViewTextBoxColumn
             // 
             this.correctAnswerDataGridViewTextBoxColumn.DataPropertyName = "CorrectAnswer";
             this.correctAnswerDataGridViewTextBoxColumn.HeaderText = "CorrectAnswer";
+            this.correctAnswerDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.correctAnswerDataGridViewTextBoxColumn.Name = "correctAnswerDataGridViewTextBoxColumn";
+            this.correctAnswerDataGridViewTextBoxColumn.Width = 125;
             // 
             // pointsDataGridViewTextBoxColumn
             // 
             this.pointsDataGridViewTextBoxColumn.DataPropertyName = "Points";
             this.pointsDataGridViewTextBoxColumn.HeaderText = "Points";
+            this.pointsDataGridViewTextBoxColumn.MinimumWidth = 6;
             this.pointsDataGridViewTextBoxColumn.Name = "pointsDataGridViewTextBoxColumn";
+            this.pointsDataGridViewTextBoxColumn.Width = 125;
             // 
             // questionsBindingSource
             // 
@@ -135,44 +178,210 @@
             // 
             this.btnCreate.BackColor = System.Drawing.Color.IndianRed;
             this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnCreate.Location = new System.Drawing.Point(27, 207);
+            this.btnCreate.Location = new System.Drawing.Point(833, 171);
+            this.btnCreate.Margin = new System.Windows.Forms.Padding(4);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(166, 83);
+            this.btnCreate.Size = new System.Drawing.Size(221, 60);
             this.btnCreate.TabIndex = 1;
             this.btnCreate.Text = "Create";
             this.btnCreate.UseVisualStyleBackColor = false;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
             // btnUpdate
             // 
             this.btnUpdate.BackColor = System.Drawing.Color.Lime;
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnUpdate.Location = new System.Drawing.Point(230, 207);
+            this.btnUpdate.Location = new System.Drawing.Point(833, 248);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(166, 83);
+            this.btnUpdate.Size = new System.Drawing.Size(221, 70);
             this.btnUpdate.TabIndex = 2;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnDelete
             // 
             this.btnDelete.BackColor = System.Drawing.Color.Turquoise;
             this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnDelete.Location = new System.Drawing.Point(441, 207);
+            this.btnDelete.Location = new System.Drawing.Point(833, 331);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(166, 83);
+            this.btnDelete.Size = new System.Drawing.Size(221, 65);
             this.btnDelete.TabIndex = 3;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.QuestionsTableAdapter = this.questionsTableAdapter;
+            this.tableAdapterManager.UpdateOrder = StaniBogat.Stani_Bogat_11d_2025DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // idLabel
+            // 
+            idLabel.AutoSize = true;
+            idLabel.Location = new System.Drawing.Point(12, 162);
+            idLabel.Name = "idLabel";
+            idLabel.Size = new System.Drawing.Size(21, 16);
+            idLabel.TabIndex = 12;
+            idLabel.Text = "Id:";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "Id", true));
+            this.idTextBox.Location = new System.Drawing.Point(77, 159);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 22);
+            this.idTextBox.TabIndex = 13;
+            // 
+            // questionLabel
+            // 
+            questionLabel.AutoSize = true;
+            questionLabel.Location = new System.Drawing.Point(8, 195);
+            questionLabel.Name = "questionLabel";
+            questionLabel.Size = new System.Drawing.Size(63, 16);
+            questionLabel.TabIndex = 14;
+            questionLabel.Text = "Question:";
+            // 
+            // questionTextBox
+            // 
+            this.questionTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "Question", true));
+            this.questionTextBox.Location = new System.Drawing.Point(77, 194);
+            this.questionTextBox.Name = "questionTextBox";
+            this.questionTextBox.Size = new System.Drawing.Size(100, 22);
+            this.questionTextBox.TabIndex = 15;
+            // 
+            // ansALabel
+            // 
+            ansALabel.AutoSize = true;
+            ansALabel.Location = new System.Drawing.Point(12, 228);
+            ansALabel.Name = "ansALabel";
+            ansALabel.Size = new System.Drawing.Size(45, 16);
+            ansALabel.TabIndex = 16;
+            ansALabel.Text = "Ans A:";
+            // 
+            // ansATextBox
+            // 
+            this.ansATextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "AnsA", true));
+            this.ansATextBox.Location = new System.Drawing.Point(77, 231);
+            this.ansATextBox.Name = "ansATextBox";
+            this.ansATextBox.Size = new System.Drawing.Size(100, 22);
+            this.ansATextBox.TabIndex = 17;
+            // 
+            // ansBLabel
+            // 
+            ansBLabel.AutoSize = true;
+            ansBLabel.Location = new System.Drawing.Point(12, 265);
+            ansBLabel.Name = "ansBLabel";
+            ansBLabel.Size = new System.Drawing.Size(45, 16);
+            ansBLabel.TabIndex = 18;
+            ansBLabel.Text = "Ans B:";
+            // 
+            // ansBTextBox
+            // 
+            this.ansBTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "AnsB", true));
+            this.ansBTextBox.Location = new System.Drawing.Point(77, 265);
+            this.ansBTextBox.Name = "ansBTextBox";
+            this.ansBTextBox.Size = new System.Drawing.Size(100, 22);
+            this.ansBTextBox.TabIndex = 19;
+            // 
+            // ansCLabel
+            // 
+            ansCLabel.AutoSize = true;
+            ansCLabel.Location = new System.Drawing.Point(12, 302);
+            ansCLabel.Name = "ansCLabel";
+            ansCLabel.Size = new System.Drawing.Size(45, 16);
+            ansCLabel.TabIndex = 20;
+            ansCLabel.Text = "Ans C:";
+            // 
+            // ansCTextBox
+            // 
+            this.ansCTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "AnsC", true));
+            this.ansCTextBox.Location = new System.Drawing.Point(77, 299);
+            this.ansCTextBox.Name = "ansCTextBox";
+            this.ansCTextBox.Size = new System.Drawing.Size(100, 22);
+            this.ansCTextBox.TabIndex = 21;
+            // 
+            // ansDLabel
+            // 
+            ansDLabel.AutoSize = true;
+            ansDLabel.Location = new System.Drawing.Point(12, 331);
+            ansDLabel.Name = "ansDLabel";
+            ansDLabel.Size = new System.Drawing.Size(46, 16);
+            ansDLabel.TabIndex = 22;
+            ansDLabel.Text = "Ans D:";
+            // 
+            // ansDTextBox
+            // 
+            this.ansDTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "AnsD", true));
+            this.ansDTextBox.Location = new System.Drawing.Point(77, 328);
+            this.ansDTextBox.Name = "ansDTextBox";
+            this.ansDTextBox.Size = new System.Drawing.Size(100, 22);
+            this.ansDTextBox.TabIndex = 23;
+            // 
+            // correctAnswerLabel
+            // 
+            correctAnswerLabel.AutoSize = true;
+            correctAnswerLabel.Location = new System.Drawing.Point(10, 363);
+            correctAnswerLabel.Name = "correctAnswerLabel";
+            correctAnswerLabel.Size = new System.Drawing.Size(100, 16);
+            correctAnswerLabel.TabIndex = 24;
+            correctAnswerLabel.Text = "Correct Answer:";
+            // 
+            // correctAnswerTextBox
+            // 
+            this.correctAnswerTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "CorrectAnswer", true));
+            this.correctAnswerTextBox.Location = new System.Drawing.Point(116, 357);
+            this.correctAnswerTextBox.Name = "correctAnswerTextBox";
+            this.correctAnswerTextBox.Size = new System.Drawing.Size(100, 22);
+            this.correctAnswerTextBox.TabIndex = 25;
+            // 
+            // pointsLabel
+            // 
+            pointsLabel.AutoSize = true;
+            pointsLabel.Location = new System.Drawing.Point(12, 393);
+            pointsLabel.Name = "pointsLabel";
+            pointsLabel.Size = new System.Drawing.Size(47, 16);
+            pointsLabel.TabIndex = 26;
+            pointsLabel.Text = "Points:";
+            pointsLabel.Click += new System.EventHandler(this.pointsLabel_Click);
+            // 
+            // pointsTextBox
+            // 
+            this.pointsTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.questionsBindingSource, "Points", true));
+            this.pointsTextBox.Location = new System.Drawing.Point(77, 393);
+            this.pointsTextBox.Name = "pointsTextBox";
+            this.pointsTextBox.Size = new System.Drawing.Size(100, 22);
+            this.pointsTextBox.TabIndex = 27;
             // 
             // PulneneNaVaprosi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 302);
+            this.ClientSize = new System.Drawing.Size(1093, 428);
+            this.Controls.Add(pointsLabel);
+            this.Controls.Add(this.pointsTextBox);
+            this.Controls.Add(correctAnswerLabel);
+            this.Controls.Add(this.correctAnswerTextBox);
+            this.Controls.Add(ansDLabel);
+            this.Controls.Add(this.ansDTextBox);
+            this.Controls.Add(ansCLabel);
+            this.Controls.Add(this.ansCTextBox);
+            this.Controls.Add(ansBLabel);
+            this.Controls.Add(this.ansBTextBox);
+            this.Controls.Add(ansALabel);
+            this.Controls.Add(this.ansATextBox);
+            this.Controls.Add(questionLabel);
+            this.Controls.Add(this.questionTextBox);
+            this.Controls.Add(idLabel);
+            this.Controls.Add(this.idTextBox);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "PulneneNaVaprosi";
             this.Text = "PulneneNaVaprosi";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PulneneNaVaprosi_FormClosing);
@@ -181,6 +390,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.questionsBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.stani_Bogat_11d_2025DataSet)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -201,5 +411,14 @@
         private System.Windows.Forms.Button btnCreate;
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnDelete;
+        private Stani_Bogat_11d_2025DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox questionTextBox;
+        private System.Windows.Forms.TextBox ansATextBox;
+        private System.Windows.Forms.TextBox ansBTextBox;
+        private System.Windows.Forms.TextBox ansCTextBox;
+        private System.Windows.Forms.TextBox ansDTextBox;
+        private System.Windows.Forms.TextBox correctAnswerTextBox;
+        private System.Windows.Forms.TextBox pointsTextBox;
     }
 }
--- StaniBogat/PulneneNaVaprosi.cs
+++ StaniBogat/PulneneNaVaprosi.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,12 @@ namespace StaniBogat
 {
     public partial class PulneneNaVaprosi : Form
     {
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Stani_Bogat_11d_2025; Integrated Security=True";
+        SqlConnection connect;
         public PulneneNaVaprosi()
         {
             InitializeComponent();
+            connect = new SqlConnection(connectionString);
         }
 
         private void PulneneNaVaprosi_Load(object sender, EventArgs e)
@@ -27,6 +32,115 @@ namespace StaniBogat
         private void PulneneNaVaprosi_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connect.Open();
+                string sql = "INSERT INTO Questions (Question, AnsA, AnsB, AnsC, AnsD, CorrectAns, Points) " +
+                             "VALUES (@question, @ansA, @ansB, @ansC, @ansD, @correctAns, @points)";
+
+                using (SqlCommand cmd = new SqlCommand(sql, connect))
+                {
+                    cmd.Parameters.AddWithValue("@question", questionTextBox.Text);
+                    cmd.Parameters.AddWithValue("@ansA", ansATextBox.Text);
+                    cmd.Parameters.AddWithValue("@ansB", ansBTextBox.Text);
+                    cmd.Parameters.AddWithValue("@ansC", ansCTextBox.Text);
+                    cmd.Parameters.AddWithValue("@ansD", ansDTextBox.Text);
+                    cmd.Parameters.AddWithValue("@correctAns", correctAnswerTextBox.Text);
+                    cmd.Parameters.AddWithValue("@points", int.Parse(pointsTextBox.Text));
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("The question has been added!");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connect.Close();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (questionTextBox.Text != "" && ansATextBox.Text != "" && ansBTextBox.Text != "" &&
+                ansCTextBox.Text != "" && ansDTextBox.Text != "" && correctAnswerTextBox.Text != "")
+            {
+                string connectionString = @"Server=(localdb)\MSSQLLocalDB; Database=Stani_Bogat_11d_2025; Integrated Security=true";
+                SqlConnection connect = new SqlConnection(connectionString);
+
+                if (connect.State == ConnectionState.Closed)
+                    connect.Open();
+
+                string sql = "UPDATE Questions " +
+                             "SET Question = @question, AnsA = @ans1, AnsB = @ans2, AnsC = @ans3, AnsD = @ans4, " +
+                             "CorrectAns = @ansCorrect, Points = @points " +
+                             "WHERE id = @id";
+
+                SqlCommand cmd = new SqlCommand(sql, connect);
+
+                cmd.Parameters.AddWithValue("@question", questionTextBox.Text);
+                cmd.Parameters.AddWithValue("@ans1", ansATextBox.Text);
+                cmd.Parameters.AddWithValue("@ans2", ansBTextBox.Text);
+                cmd.Parameters.AddWithValue("@ans3", ansCTextBox.Text);
+                cmd.Parameters.AddWithValue("@ans4", ansDTextBox.Text);
+                cmd.Parameters.AddWithValue("@ansCorrect", correctAnswerTextBox.Text);
+                cmd.Parameters.AddWithValue("@points", int.Parse(pointsTextBox.Text));
+                cmd.Parameters.AddWithValue("@id", int.Parse(idTextBox.Text));
+
+                int updatedRows = cmd.ExecuteNonQuery();
+                if (updatedRows> 0)
+                {
+                    MessageBox.Show("The question is updated!");
+                }
+                else
+                {
+                    MessageBox.Show("The question is not found!");
+                }
+
+                connect.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please enter information in all fields! ");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Server=(localdb)\MSSQLLocalDB; Database=Stani_Bogat_11d_2025; Integrated Security=true";
+            SqlConnection connect = new SqlConnection(connectionString);
+
+            if (connect.State == ConnectionState.Closed)
+                connect.Open();
+
+            string sql = "DELETE FROM Questions WHERE id = @id";
+            SqlCommand cmd = new SqlCommand(sql, connect);
+
+            cmd.Parameters.AddWithValue("@id", int.Parse(idTextBox.Text));
+
+            int deletedRows = cmd.ExecuteNonQuery();
+            if (deletedRows > 0)
+            {
+                MessageBox.Show("The question is deleted!!");
+            }
+            else
+            {
+                MessageBox.Show("There is no question like this!");
+            }
+
+            connect.Close();
+        }
+
+        private void pointsLabel_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
